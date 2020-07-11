d9ad64ef6352da0b93cc52be83d576fd2e18f36f
NamitWHJR <65145516+NamitWHJR@users.noreply.github.com>
2020-07-10 22:25:53 -0700

    Add files via upload

---
+++ JS/form.js
@@ -0,0 +1,47 @@
+class Form{
+    constructor(){
+        this.input = createInput("Name")
+        this.button = createButton("play")
+        this.greeting = createElement("h3")
+    }
+
+display(){
+var title = createElement("h2")
+ title.html("car racing game")
+ title.position(displayWidth/2 - 60, 20);
+
+
+this.input.position(displayWidth/2 - 70, displayHeight/2 - 80);
+
+
+this.button.position(displayWidth/2 + 30,displayHeight/2);
+
+this.button.mousePressed(()=>{
+    this.input.hide() 
+    this.button.hide()
+
+ player.name = this.input.value()
+
+playerCount = playerCount + 1
+
+player.index = playerCount
+
+player.update()
+player.updateCount(playerCount)
+
+
+this.greeting.html("hello " + player.name)
+this.greeting.position(displayWidth/2 -90,displayHeight/4)
+})
+
+}
+
+hide(){
+
+    this.input.hide() 
+    this.button.hide()
+    this.greeting.hide()
+
+}
+
+}
